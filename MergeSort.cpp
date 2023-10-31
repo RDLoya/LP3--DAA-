97aa8d69fffb86a299dc339c14862dabafeadc06
Rishikesh Deepak Loya <98093247+RDLoya@users.noreply.github.com>
2023-10-31 22:07:45 +0530

    DAA_Mini_Project.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,20 +1,16 @@
+#include <iostream>
 #include <vector>
 #include <thread>
-#include <chrono>   // Include the chrono library for time measurements
-using namespace std;
+#include <algorithm>
+#include <chrono>
+#include <random>
 
-// Function to merge two sorted subarrays
-void merge(vector<int>& arr, int left, int mid, int right) {
+void merge(std::vector<int>& arr, int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
-    vector<int> left_half(n1);
-    vector<int> right_half(n2);
-
-    for (int i = 0; i < n1; i++)
-        left_half[i] = arr[left + i];
-    for (int i = 0; i < n2; i++)
-        right_half[i] = arr[mid + 1 + i];
+    std::vector<int> left_half(arr.begin() + left, arr.begin() + left + n1);
+    std::vector<int> right_half(arr.begin() + mid + 1, arr.begin() + mid + 1 + n2);
 
     int i = 0, j = 0, k = left;
 
@@ -42,77 +38,63 @@ void merge(vector<int>& arr, int left, int mid, int right) {
     }
 }
 
-// Merge Sort algorithm
-void mergeSort(vector<int>& arr, int left, int right) {
+void merge_sort(std::vector<int>& arr, int left, int right) {
     if (left < right) {
         int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
+        merge_sort(arr, left, mid);
+        merge_sort(arr, mid + 1, right);
         merge(arr, left, mid, right);
     }
 }
 
-// Multithreaded merge sort
-void multiThreadedMergeSort(vector<int>& arr, int left, int right, int threads) {
-    if (threads <= 1 || (right - left + 1) <= 1000) {
-        mergeSort(arr, left, right);
-    } else {
+void merge_sort_parallel(std::vector<int>& arr, int left, int right, int depth) {
+    if (left < right) {
         int mid = left + (right - left) / 2;
 
-        thread leftThread(multiThreadedMergeSort, ref(arr), left, mid, threads / 2);
-        thread rightThread(multiThreadedMergeSort, ref(arr), mid + 1, right, threads / 2);
+        if (depth > 0) {
+            std::thread left_thread(merge_sort_parallel, std::ref(arr), left, mid, depth - 1);
+            std::thread right_thread(merge_sort_parallel, std::ref(arr), mid + 1, right, depth - 1);
 
-        leftThread.join();
-        rightThread.join();
+            left_thread.join();
+            right_thread.join();
+        } else {
+            merge_sort(arr, left, mid);
+            merge_sort(arr, mid + 1, right);
+        }
 
         merge(arr, left, mid, right);
     }
 }
 
 int main() {
-    vector<int> arr;
-    int size, threads;
+    std::vector<int> arr;
+    int n = 10000; // Adjust the size of the array
 
-    // Input array size and number of threads
-    cout << "Enter the size of the array: ";
-    cin >> size;
-
-    cout << "Enter the number of threads: ";
-    cin >> threads;
-
-    // Input array elements
-    cout << "Enter the array elements:" << endl;
-    for (int i = 0; i < size; i++) {
-        int element;
-        cin >> element;
-        arr.push_back(element);
+    // Generate a random worst-case scenario array
+    for (int i = n; i > 0; i--) {
+        arr.push_back(i);
     }
 
-    // Perform multithreaded merge sort
-    multiThreadedMergeSort(arr, 0, size - 1, threads);
+    // Measure time for standard merge sort
+    auto start = std::chrono::high_resolution_clock::now();
+    merge_sort(arr, 0, n - 1);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Standard Merge Sort Time: " << elapsed.count() << " seconds" << std::endl;
 
-    // Print the sorted array
-    cout << "Sorted array: ";
-    for (int num : arr) {
-        cout << num << " ";
-    }
-    cout << endl;
-     // Measure time for merge sort
-    auto start_time_merge = chrono::high_resolution_clock::now();
-    mergeSort(arr, 0, size - 1);
-    auto end_time_merge = chrono::high_resolution_clock::now();
-    chrono::duration<double> mergeSortTime = end_time_merge - start_time_merge;
+    // Generate a random best-case scenario array
+    std::random_device rd;
+    std::mt19937 g(rd());
+
+    std::shuffle(arr.begin(), arr.end(), g);
 
     // Measure time for multithreaded merge sort
-    auto start_time_multiThreaded = chrono::high_resolution_clock::now();
-    multiThreadedMergeSort(arr, 0, size - 1, threads);
-    auto end_time_multiThreaded = chrono::high_resolution_clock::now();
-    chrono::duration<double> multiThreadedMergeSortTime = end_time_multiThreaded - start_time_multiThreaded;
+    start = std::chrono::high_resolution_clock::now();
+    int depth = 3; // Adjust the depth for parallelism
+    merge_sort_parallel(arr, 0, n - 1, depth);
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = end - start;
+    std::cout << "Multithreaded Merge Sort Time: " << elapsed.count() << " seconds" << std::endl;
 
-    // Print the time taken for each sorting method
-    cout << "Merge Sort Time: " << mergeSortTime.count() << " seconds" << endl;
-    cout << "Multithreaded Merge Sort Time: " << multiThreadedMergeSortTime.count() << " seconds" << endl;
     return 0;
 }
